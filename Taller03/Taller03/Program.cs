7c0a93a7719dbc3581a7cb44850a187d33d8ec31
nelsonA113 <nelsonsanchezgomez@outlook.es>
2025-03-16 21:18:45 -0500

    actividad03

---
+++ Taller03/Taller03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //variable de solicitud
+        Console.Write("introduce el tamaño (n) de la matriz: ");
+        int n = int.Parse(Console.ReadLine());
+        //matriz y relleno mediante formula celda
+        int[,] matriz = new int[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+               matriz[i, j] = i + j;
+            }
+        }
+        //print matriz llena
+        Console.WriteLine("\nMatriz llenna" );
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        //print triangulo
+        Console.WriteLine("\ntriangular inferior");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (j <= i )
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                else
+                {
+                    Console.Write("\t");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ taller03punto2/taller03punto2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        //limite
+        int N = 11;
+        //matriz
+        int[,] matriz = new int[N, N];
+        //numeros relleno
+        int numero = 1;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                matriz[i, j] = numero;
+                numero++;
+            }
+        }
+            Console.WriteLine("matriz completa");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(matriz[i, j].ToString().PadLeft(4));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine() ;
+
+            Console.WriteLine("El Reloj");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0 ; j < N ; j++)
+                {
+                    if (j >= i && j < N - i || j <= i && j >= N - i - 1)
+
+                    {
+                       Console.Write(matriz[i,j].ToString().PadLeft(4));
+                    }
+                    else
+                    {
+                        Console.Write("    ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        
+    }
+}
